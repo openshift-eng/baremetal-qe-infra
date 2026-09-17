9ffd5d32ecfa6a2b1371a65ec55bbfe596f9b467
Manoj Hans <mhans@redhat.com>
2026-09-17 15:23:06 +0200

    Encode YAML file contents as Base64 (#129)

--- images/fcos-auxiliary-image/root/usr/bin/patch_ove_static_network_ignition_file.sh
+++ images/fcos-auxiliary-image/root/usr/bin/patch_ove_static_network_ignition_file.sh
@@ -2,26 +2,21 @@
 set -euo pipefail
 
 OVE_ISO_NAME="${1:-}"
-PULL_SECRET="${2:-}"
-INSTALL_CONFIG="${3:-}"
-AGENT_CONFIG="${4:-}"
+INSTALL_CONFIG="${2:-}"
+AGENT_CONFIG="${3:-}"
 
 [ -z "$OVE_ISO_NAME" ] && { echo "Error: OVE_ISO_NAME is not provided."; exit 1; }
-[ -z "$PULL_SECRET" ] && { echo "Error: PULL_SECRET is not provided."; exit 1; }
 [ -z "$INSTALL_CONFIG" ] && { echo "Error: INSTALL_CONFIG is not provided."; exit 1; }
 [ -z "$AGENT_CONFIG" ] && { echo "Error: AGENT_CONFIG is not provided."; exit 1; }
 
 WORKING_DIR="/tmp/$OVE_ISO_NAME"
-PULL_SECRET_PATH="/tmp/pull-secret"
+PULL_SECRET_PATH="/$WORKING_DIR/pull-secret"
 
 mkdir -p "$WORKING_DIR"
 
-echo "$INSTALL_CONFIG" > "$WORKING_DIR/install-config.yaml"
-echo "$AGENT_CONFIG" > "$WORKING_DIR/agent-config.yaml"
-
-if [ ! -f "$PULL_SECRET_PATH" ]; then
-    echo "$PULL_SECRET" > "$PULL_SECRET_PATH"
-fi
+echo "$INSTALL_CONFIG" | base64 -d > "$WORKING_DIR/install-config.yaml"
+echo "$AGENT_CONFIG" | base64 -d > "$WORKING_DIR/agent-config.yaml"
+yq ".pullSecret" "$WORKING_DIR/install-config.yaml" > "$PULL_SECRET_PATH"
 
 OVE_ISO_PATH="/var/mnt/data-storage/html/$OVE_ISO_NAME"
 
@@ -48,12 +43,13 @@ rm -f "$WORKING_DIR"/.openshift*
 
 echo "Generating unconfigured ignition file..."
 "$WORKING_DIR/openshift-install" agent create unconfigured-ignition \
-    --dir "$WORKING_DIR"
+    --dir "$WORKING_DIR" --log-level info
 
 echo "Replacing ignition file..."
 coreos-installer iso ignition embed \
     -i "$WORKING_DIR/unconfigured-agent.ign" \
     -f "$OVE_ISO_PATH"
 
+echo "Removing $WORKING_DIR directory..."
 rm -rf "$WORKING_DIR"
 echo "Done!"
